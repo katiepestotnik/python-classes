53c65dcebef8eb3642e5061c7ff60beb8cc82b56
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-06-28 22:46:56 -0600

    finish bank classes

--- python-classes.py
+++ python-classes.py
@@ -14,7 +14,7 @@ nums = [1, 2, 3, 8, 2, 4, 0]
 # print all of the properties of list object
 #print(dir(nums))
 
-# dog class
+# dog class superclass
 class Dog():
     # class attribute - creates an id for each instance of Dog()
     next_id = 1
@@ -57,6 +57,35 @@ print(klondike)
 # check total dogs
 print(f'Total Dogs = {Dog.get_total_dogs()}')
 
+# inheritance
+# has all the attribute /methods of Dog but more specialized
+#sub class
+class ShowDog(Dog):
+  # Add additional parameters AFTER those in the superclass
+  def __init__(self, name, age = 0, total_earnings = 0):
+    # Always call the superclass's __init__ first
+    Dog.__init__(self, name, age)
+    # Now add any new attributes
+    self.total_earnings = total_earnings
+  
+  # Add additional methods
+  def add_prize_money(self, amount):
+    self.total_earnings += amount
+
+star = ShowDog('Star', 1, 2000)
+print(star)
+star.add_prize_money(3000)
+print(f'Show dog total earnings: {star.total_earnings}')
+
+
+
+
+
+
+
+
+
+
 class Vehicle():
     def __init__(self, vin, make, model, running = False):
         self.vin = vin
@@ -86,3 +115,42 @@ print(car)
 car.stop()
 print(car)
 
+
+
+# bank account classes
+import random
+class BankAccount():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+        self.account_no = random.randint(1000, 9000)
+    def __str__(self):
+        return f'Account {self.account_no} Balance: {self.balance}'
+    
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        self.balance -= amount
+
+katie = BankAccount('Katie', 1000)
+print(katie)
+katie.deposit(300)
+print(katie)
+katie.withdraw(1000)
+print(katie)
+
+class SavingsAccount(BankAccount):
+    def __init__(self, owner, balance, has_overdraft=False):
+        self.has_overdraft = has_overdraft
+        BankAccount.__init__(self, owner, balance)
+    def withdraw(self, amount):
+        if self.has_overdraft:   
+            self.balance -= amount
+        else:
+            print('No withdraws allowed')
+            return self.balance
+
+save = SavingsAccount('katie', 500, True)
+print(save)
+save.withdraw(100)
+print(save)
